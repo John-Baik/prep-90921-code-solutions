7c34b8294c90ed407767ae10d98486958cfbee78
John Baik <johnwbaik@gmail.com>
2021-09-24 15:34:18 -0700

    Updated Arrays code

--- prep-javascript-arrays/main.js
+++ prep-javascript-arrays/main.js
@@ -17,5 +17,5 @@ console.log("the 3rd element of the array is" + " " + thirdElement);
 var length = languages.length;
 console.log("The length of the array is" + " " + length);
 
-var lastElement = languages[5];
+var lastElement = length - 1;
 console.log("The last element of the array is" + " " + lastElement);
